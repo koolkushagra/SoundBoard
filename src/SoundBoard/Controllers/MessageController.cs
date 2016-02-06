692181c8de86951e12bfe6e1b5645784910a80cf
Kushagra Vaish <vaish.kushagra@gmail.com>
2016-02-06 16:37:55 +0530

    Initial Commit

---
+++ src/SoundBoard/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SoundBoard.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // /message/create
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string messageTitle, string messageContent) {
+
+            //todo: validate
+
+            //todo: store
+
+            return View();
+        }
+    }
+}
